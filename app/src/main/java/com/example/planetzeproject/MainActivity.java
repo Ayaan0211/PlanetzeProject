fe1874f2f530e65c41588837996932409a9a70b3
[Max <Max.tyu.ju@gmail.com>
2024-11-08 16:19:40 -0500

    feat: created welcome page and splash page skeleton

--- app/src/main/java/com/example/planetzeproject/MainActivity.java
+++ app/src/main/java/com/example/planetzeproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.planetzeproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button login;
+    private Button signup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,26 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+               startActivity(intent);
+               finish();
+           }
+        });
+
+        signup = findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
