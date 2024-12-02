e18aa0a9a9b7ff389bd63f6aa54146fdbe495bd2
[Max <Max.tyu.ju@gmail.com>
2024-12-01 23:48:27 -0500

    chore: added refactored login code

--- app/src/main/java/com/example/planetzeproject/LoginActivity.java
+++ app/src/main/java/com/example/planetzeproject/LoginActivity.java
@@ -1,8 +1,8 @@
 package com.example.planetzeproject;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -10,21 +10,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private LoginPresenter presenter;
     TextInputEditText editTextEmail, editTextPassword;
     Button buttonLogin, back;
-    FirebaseAuth mAuth;
     ProgressBar progressBar;
     TextView textView, textView2;
 
@@ -32,22 +26,34 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
+        if (presenter.checkLoggedIn() != null) {
             Intent intent = new Intent(getApplicationContext(), EcoGaugeActivity.class);
             startActivity(intent);
             finish();
         }
     }
 
+    public void login(Context context) {
+        System.out.println("WORKS!");
+        Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(context, EcoGaugeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
+    public void invalidLogin(Context context) {
+        Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        presenter = new LoginPresenter(new LoginModel(), this);
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
-        mAuth = FirebaseAuth.getInstance();
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
         buttonLogin = findViewById(R.id.btn_login);
@@ -95,33 +101,21 @@ public class LoginActivity extends AppCompatActivity {
                 email = String.valueOf(editTextEmail.getText());
                 password = String.valueOf(editTextPassword.getText());
 
-                if (TextUtils.isEmpty(email)) {
+
+                if (presenter.emailEmpty(email)) {
                     Toast.makeText(LoginActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
                     return;
                 }
 
-                if (TextUtils.isEmpty(password)) {
+                if (presenter.passwordEmpty(password)) {
                     Toast.makeText(LoginActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
                     return;
                 }
 
-                mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                progressBar.setVisibility(View.GONE);
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(getApplicationContext(), EcoGaugeActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
+                presenter.signIn(email, password, getApplicationContext());
+                progressBar.setVisibility(View.GONE);
             }
         });
 
