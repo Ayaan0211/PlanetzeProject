e383964b403ecc6ee06eb526a07ae8700dc0ab0e
Villagershrm <villagershrm@gmail.com>
2024-12-03 14:56:33 -0500

    feat: created the keyword searcher

--- app/src/main/java/com/example/planetzeproject/HabitSearcher.java
+++ app/src/main/java/com/example/planetzeproject/HabitSearcher.java
@@ -1,4 +1,9 @@
 package com.example.planetzeproject;
 
-public interface HabitSearcher {
+import java.util.List;
+
+public abstract class HabitSearcher {
+    protected List<String> habits;
+    //Returns an ArrayList<String> of habits that match
+    public abstract List<String> search();
 }
